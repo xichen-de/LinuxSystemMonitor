a7b2c843627226101056ffb4d435113d98bf0d8e
Xi Chen <x.chen0724@gmail.com>
2021-11-02 11:15:21 +0100

    Add OS and kernel display.

--- src/system.cpp
+++ src/system.cpp
@@ -1,4 +1,8 @@
+#include "system.h"
+
+#include <linux_parser.h>
 #include <unistd.h>
+
 #include <cstddef>
 #include <set>
 #include <string>
@@ -6,7 +10,6 @@
 
 #include "process.h"
 #include "processor.h"
-#include "system.h"
 
 using std::set;
 using std::size_t;
@@ -20,13 +23,13 @@ Processor& System::Cpu() { return cpu_; }
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
+std::string System::Kernel() { return LinuxParser::Kernel(); }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
